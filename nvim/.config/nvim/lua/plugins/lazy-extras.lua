775ca8c852c0c6b44255a9053078aa42feabfe56
Rojan Rajbhandari <rrojan20@gmail.com>
2024-08-20 08:34:28 +0545

    Add lsp for golang and prisma

--- nvim/.config/nvim/lua/plugins/lazy-extras.lua
+++ nvim/.config/nvim/lua/plugins/lazy-extras.lua
@@ -1,5 +1,16 @@
 return {
+  -- lang
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.prisma" },
+
+  -- linting
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+
+  -- formatting
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+  -- editor
   { import = "lazyvim.plugins.extras.editor.mini-move" },
 }
