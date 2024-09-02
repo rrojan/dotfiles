d0683e5fd43ec11e4f0c17a3cbd9e5bd31985f23
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-02 19:29:55 +0545

    Remap Hover from K to M

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -6,6 +6,7 @@ return {
   init = function()
     local lsp_keys = require("lazyvim.plugins.lsp.keymaps")
     local keys = lsp_keys.get()
+    -- Remap Hover from default K to M
     for _, key in ipairs(keys) do
       if key[1] == "K" then
         key[1] = "M"
