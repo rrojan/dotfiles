ed42b0c344501dd2df6fe7cd882e5f5c6bc4f5ca
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-13 10:43:22 +0545

    Add tailwind support for lazyvim

--- nvim/.config/nvim/lua/plugins/lazy-extras.lua
+++ nvim/.config/nvim/lua/plugins/lazy-extras.lua
@@ -4,6 +4,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.prisma" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
 
   -- linting
   { import = "lazyvim.plugins.extras.linting.eslint" },
