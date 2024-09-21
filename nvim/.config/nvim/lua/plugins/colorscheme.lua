34aab576c24d18e5730ed6c7b0293d0eee23c6ac
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-21 16:15:13 +0545

    Comment out unused themes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -8,28 +8,28 @@ return {
   },
 
   -- Colorschemes
-  {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false,
-    },
-  },
+  -- {
+  --   "eldritch-theme/eldritch.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent = false,
+  --   },
+  -- },
   {
     "EdenEast/nightfox.nvim",
     lazy = false,
   },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    -- priority = 1000,
-    opts = {},
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    -- priority = 1000,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
 }
