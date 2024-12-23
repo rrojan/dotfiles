b2ed4107188608148698e4f63b1fbea3d4dc8847
Rojan Rajbhandari <rrojan20@gmail.com>
2024-12-23 13:28:05 +0545

    Reenable telescope instead of fzf-lua

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+  },
+
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      -- still need a home level `.ignore` to include .env files
+      hidden = true,
+      file_ignore_patterns = { ".git/", "**/node_modules/" },
+    },
+  },
+}
