a58025cd1dadf2c036b12dcbcc1f1aa23e11c8e3
Rojan Rajbhandari <rrojan20@gmail.com>
2024-12-24 21:32:37 +0545

    Remove render-markdown plugin

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
-  opts = {},
-}
