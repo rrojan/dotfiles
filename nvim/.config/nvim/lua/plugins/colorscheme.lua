13b9adf4213c55a37907cd46656e893f27b6731c
Rojan Rajbhandari <rrojan20@gmail.com>
2025-01-12 07:49:03 +0545

    Update plugins

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,12 +7,6 @@ return {
     },
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-  },
-  {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
