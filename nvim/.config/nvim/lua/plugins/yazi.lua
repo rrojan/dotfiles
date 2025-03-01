c2173a5e37f5a4c7118dd7df08f5387fd7c81c77
Rojan Rajbhandari <roj@Rojans-MacBook-Air.local>
2025-03-01 19:25:06 +0545

    Add yazi plugin browser for nvim

---
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,30 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = { "folke/snacks.nvim", lazy = true },
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
