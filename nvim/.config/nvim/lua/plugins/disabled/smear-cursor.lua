be5c693c0d6512968a7072ef86355d1651d16fd9
Rojan Rajbhandari <roj@Rojans-MacBook-Air.local>
2025-03-02 08:35:41 +0545

    Disable smear cursor

---
+++ nvim/.config/nvim/lua/plugins/disabled/smear-cursor.lua
@@ -0,0 +1,12 @@
+if true then
+  return {}
+end
+
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    distance_stop_animating = 0.5,
+  },
+}
