2ad57495df2f8305656b611b6242e2e2e2a259e5
Rojan Rajbhandari <rrojan20@gmail.com>
2025-03-29 20:38:19 +0545

    Add augment.nvim (context engine + completions + chat)

---
+++ nvim/.config/nvim/lua/plugins/augment.lua
@@ -0,0 +1,16 @@
+vim.g.augment_workspace_folders = { "/Users/roj/Documents/Outside/tasks-app" }
+
+return {
+  "augmentcode/augment.vim",
+  config = function()
+    -- Normal and visual mode mapping to send a chat message
+    vim.api.nvim_set_keymap("n", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+
+    -- Start a new chat conversation
+    vim.api.nvim_set_keymap("n", "<leader>an", ":Augment chat-new<CR>", { noremap = true, silent = true })
+
+    -- Toggle chat panel visibility
+    vim.api.nvim_set_keymap("n", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
+  end,
+}
