c75fd1b701a95aebcec3dbdc9553b52a9d1a64e6
Rojan Rajbhandari <rrojan20@gmail.com>
2025-10-26 11:52:42 +0545

    Fix deprecated lazyvim.plugins.lsp.keymaps usage

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,14 +3,26 @@ return {
   opts = {
     inlay_hints = { enabled = false },
   },
-  init = function()
-    local lsp_keys = require("lazyvim.plugins.lsp.keymaps")
-    local keys = lsp_keys.get()
-    -- Remap Hover from default K to M
-    for _, key in ipairs(keys) do
-      if key[1] == "K" then
-        key[1] = "M"
-      end
-    end
+  config = function(_, opts)
+    require("lspconfig").setup(opts)
+
+    LazyVim.on_attach(function(_, buffer)
+      local map = vim.keymap.set
+      local wk = require("which-key")
+
+      -- Unmap default "K" (hover)
+      pcall(vim.keymap.del, "n", "K", { buffer = buffer })
+
+      map("n", "M", vim.lsp.buf.hover, { buffer = buffer, desc = "LSP Hover" })
+
+      map("n", "K", "{", { desc = "Move up paragraph" })
+      map("n", "J", "}", { desc = "Move down paragraph" })
+
+      wk.add({
+        ["M"] = { "LSP Hover" },
+        ["K"] = { "Move up paragraph" },
+        ["J"] = { "Move down paragraph" },
+      })
+    end)
   end,
 }
